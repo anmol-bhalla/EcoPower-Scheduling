37b5c506709dad989cac0c4683b86915c5f9ac0a
Anmol Bhalla <anmolbhalla21@gmail.com>
2023-11-17 17:12:12 -0800

    Revert "Finish posting local edits"
    This reverts commit e62154bda374e37de7df45201a9a6adfb195c955.

--- app.py
+++ app.py
@@ -1,49 +1,7 @@
 from flask import Flask, jsonify
-from flask import request
-from flask_sqlalchemy import SQLAlchemy
+from functions import utilities as utils
 
 app = Flask(__name__)
-
-
-#stuff added on nov 1:
-
-#DATABASE_URL = 'postgresql://myappuser:password@localhost/schedulingdb'
-DATABASE_URL= 'postgresql://bqafdzmwdkhjrn:07623061f89a9b916aae8121938773b6e6020aae7688a93c875f301960aeb4bd@ec2-34-202-53-101.compute-1.amazonaws.com:5432/df70jf2v65kau1'
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
-db = SQLAlchemy(app)
-
-#add info about possible tables eg: technician, calender
-
-class Technician(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-class Appointment(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    time = db.Column(db.String(80), unique=True, nullable=False)
-    confirmation_status = db.Column(db.Integer, db.ForeignKey('technician.id'), nullable=False)
-
-
-#Implement the post requests of your APIs
-
-@app.route('/api/technician', methods=['POST'])
-def register_technician():
-    data = request.json
-    new_tech = Technician(username=data['username'], email=data['email'])
-    db.session.add(new_tech)
-    db.session.commit()
-    return jsonify({'message': 'Technician registered successfully!'})
-
-@app.route('/api/appointment', methods=['POST'])
-def appointment_setup():
-    data = request.json
-    new_appointment = Appointment(time=data['time'], confirmation_status=data['confirmation_status'])
-    db.session.add(new_appointment)
-    db.session.commit()
-    return jsonify({'message': 'Appoitment updated successfully!'})
-
-#stuff added for nov 1 ends**
 
 @app.route("/")
 def root():
@@ -100,9 +58,3 @@ def get_user(username):
 def get_user_devices(username, deviceid):
     # TO DO: get list of user devices
     return jsonify({'username': username, 'deviceid': deviceid})
-
-
-#getting flask to run:
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
